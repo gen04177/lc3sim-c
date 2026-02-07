8f09ec5d6bb5c55da117e19f88267d06e7c1330b
gen04177 <159953537+gen04177@users.noreply.github.com>
2026-02-06 21:10:47 -0300

    Update libretro.c

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -26,6 +26,9 @@ static uint32_t *framebuffer;
 static retro_audio_sample_t audio_cb;
 static retro_audio_sample_batch_t audio_batch_cb;
 
+static retro_input_poll_t input_poll_cb;
+static retro_input_state_t input_state_cb;
+
 static void lc3_putchar(uint16_t v)
 {
     unsigned char c = v & 0xFF;
@@ -148,8 +151,6 @@ static bool vm_halted = false;
 
 static retro_environment_t environ_cb;
 static retro_video_refresh_t video_cb;
-static retro_input_poll_t input_poll_cb;
-static retro_input_state_t input_state_cb;
 static retro_log_printf_t log_cb;
 
 static void fallback_log(enum retro_log_level level,
